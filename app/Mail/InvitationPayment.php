c165e722874e514df244ef4dfd8abb78dc44e876
Lerian Febriana <kanglerian@gmail.com>
2024-08-19 20:29:28 +0700

    teks

--- app/Mail/InvitationPayment.php
+++ app/Mail/InvitationPayment.php
@@ -27,7 +27,7 @@ class InvitationPayment extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Invitation Payment',
+            subject: 'Pembayaran Undangan Pernikahan Digital',
         );
     }
 
